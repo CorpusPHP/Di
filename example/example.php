11b85483c67c19e7361e736bc99cad8f71085a3f
Jesse Donat <donatj@gmail.com>
2018-09-18 12:00:12 -0500

    Updates README.md

--- example/example.php
+++ example/example.php
@@ -7,33 +7,60 @@ $di = new \Corpus\Di\Di();
 // Eager Loading
 $di->set('foo', new Foo());
 
+$di->get('foo'); // the Foo instance from above
+
+// --- --- --- --- --- ---
+
 // Lazy Loading
 $di->set('bar', function () {
 	return new Bar();
 });
 
+// Value is memoized, new Bar() is only called once at first `get`.
+$bar1 = $di->get('bar');
+$bar2 = $di->get('bar');
+
+// --- --- --- --- --- ---
+
 // Constructor Parameters
 $di->set('baz', function ( $qux ) {
-	return new Bar($qux);
+	return new Baz($qux);
 });
-
-// Value is memoized, new Bar() is only called once at first `get`.
-$bar  = $di->get('bar');
-$bar2 = $di->get('bar');
 
 // Calling getNew explicitly avoids the memoization. Constructor params passed as array.
 $baz  = $di->getNew('baz', [ 'corge' ]);
 $baz2 = $di->getNew('baz', [ 'grault' ]);
 
-// getMany lets you retreive multiple memoized values at once.
+// --- --- --- --- --- ---
+
+// Auto-Constructor Parametrization
+$di->set('qux', Qux::class);
+
+$qux1 = $di->get('qux'); // New instance of Qux
+$qux2 = $di->get('qux'); // Memoized instance of Qux
+
+// --- --- --- --- --- ---
+
+// Lazy Loading with auto-arguments.
+$di->set('quux', function ( Qux $qux ) {
+	return new Quux($qux);
+});
+
+$quux = $di->get('quux'); // Instance of Quux given the previous instance of Qux automatically
+
+// --- --- --- --- --- ---
+
+// getMany lets you retrieve multiple memoized values at once.
 list($foo, $bar) = $di->getMany([ 'foo', 'bar' ]);
 
-// getManyNew lets you retreive multiple new values at once, providing for arguments.
+// getManyNew lets you retrieve multiple new values at once, providing for arguments.
 list($baz, $baz2) = $di->getManyNew([ [ 'baz', [ 'corge' ] ], [ 'baz', [ 'grault' ] ] ]);
 
-// callFromReflectiveParams lets you execute a callable reflectively using the parameter name as the key
-$di->callFromReflectiveParams(function ( Foo $foo, Bar $bar ) { /* ... */ });
+$di->callFromReflectiveParams(function(Bar $bar, Baz $baz){
+	// Callable called with parameters automatically populated based on their name
+	// $bar => 'bar'
+});
 
-// constructFromReflectiveParams works like callFromReflectiveParams except on a constructor,
-// returning an instance of the requested class
-$bazInst = $di->constructFromReflectiveParams("Baz" /* Baz::class works in 5.5+ */);
+// Construct a class auto-populating constructor parameters based on their name
+$controller1 = $di->constructFromReflectiveParams(MyController::class);
+$controller2 = $di->constructFromReflectiveParams('MyController');
